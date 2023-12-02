d3e57d92d7e85a29e2fe63e5729bdc32e1e68b5f
TheOtherDoctor <rockingrooster@hotmail.com>
2023-12-03 00:37:40 +0100

    clean up as solution for day01 part2.

--- day01-part2.pl
+++ day01-part2.pl
@@ -1,17 +1,15 @@
 #!/usr/bin/perl -w
-open(inp,"day01-demo") or die;
+open(inp,"day01-input") or die;
 my $sum=0;
+# numberwords:
 my @nw=qw( one two three four five six seven eight nine );
+# loop over input and grab number from each line:
 while(<inp>){
   my $n=0;
   chomp();
   print "$_ -> ";
   # replace all number words by digits;
-  # 0. there are only few special overlap cases:
-  # searching digit from left: ( eightwo=>82, eighthree=>83, sevenine=>79, nineight=>98, twone=>2 );
-  # searching digit from right: ( twone=>1, eightwo=>2, eighthree=>3, sevenine=>9, nineight=>8 );
-  # approach: clean these cases with minimum effort...
-  # 1. prepare the ugly interlink cases:
+  # 1. fix the few ugly interlink cases with minimum effort:
   s/eight/eeightt/;
   s/one/oonee/;
   s/two/ttwoo/;
@@ -30,10 +28,10 @@ while(<inp>){
   if($_=~m/(\d)[^\d]*$/){$n=$n+$1;}
   # first digit (might be the same one), take *10:
   if($_=~m/^[^\d]*(\d)/){$n=$n+$1*10;}
+  # number done.
   print "$n.\n";
   $sum=$sum+$n;
   }
 close(inp);
 print "---> sum is $sum\n";
-# bye.
 exit(0);
