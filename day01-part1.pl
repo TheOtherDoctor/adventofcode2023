6e881c25a13b76e05087bd44d5371a25cb3f6a9b
TheOtherDoctor <rockingrooster@hotmail.com>
2023-12-02 23:00:12 +0100

    no deco.

--- day01-part1.pl
+++ day01-part1.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/perl -w
+# AoC 2023 day01 part 1. CarpeNoctem.
 open(inp,"day01-input") or die;
 my $sum=0;
 while(<inp>){
   my $n=0;
   chomp();
-  my $s=$_;
   if(m/(\d)[^\d]*$/){$n=$n+$1;}
   if(m/^[^\d]*(\d)/){$n=$n+$1*10;}
   print "num is $n\n";
@@ -12,5 +12,4 @@ while(<inp>){
   }
 close(inp);
 print "---> sum is $sum\n";
-# bye.
 exit(0);
